30438e6f93f3b4092a950e8f3b23996acca58615
Thomas Hahn <Thahn@tcbtech.com>
2018-02-22 22:38:27 -0800

    Error handling and total cardinality

--- dnsmon.py
+++ dnsmon.py
@@ -9,7 +9,9 @@ import dns.resolver
 from prometheus_client import start_http_server, Gauge, Counter
 
 records = Gauge('dns_records', 'Number of DNS records associated with a domain', ['host', 'type'])
+cardinality = Gauge('dns_cardinality', 'Lifetime (of this process) cardinality of DNS results', ['host', 'type']) 
 delta = Counter('dns_delta', 'Number of items that have changed in our queries', ['host', 'type']) 
+errors = Counter('dns_errors', 'Resolution errors encountered', ['host', 'type']) 
 
 def main():
     parser = argparse.ArgumentParser()
@@ -22,18 +24,27 @@ def main():
     resolver = dns.resolver.Resolver()
 
     lastresults = {}
+    allresults = {}
 
     start_http_server(args.port)
 
     while True:
         for host in args.hosts:
-            result = set(resolver.query(host))
+            try:
+                result = set(resolver.query(host))
+            except e:
+                errors.labels(host=host, type='A').inc(1)
+                result = set()
             records.labels(host=host, type='A').set(len(result))
             if host not in lastresults:     # Initialization
                 lastresults[host] = result
+                allresults[host] = result
             delta.labels(host=host, type='A').inc(len(lastresults[host].symmetric_difference(result)))
             lastresults[host] = result
-            print(result)
+            allresults[host] = allresults[host] | result
+            cardinality.labels(host=host, type='A').set(len(allresults[host]))
+
+            print('Records for %s: %s' % (host, result))
         sleep(args.sleep)
             
 
